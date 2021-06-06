e708156e327b0f1477127593478602dfbbba157d
Carlos Bueno Topke <carlos-bueno-topke@hotmail.com>
2021-06-06 01:30:32 -0500

    Agregamos el KNN

---
+++ KNN.py
@@ -0,0 +1,21 @@
+import math
+import codecs
+from operator import itemgetter
+from statistics import mode
+# Definimos la función de distancia euclidiana 
+def distancia(lista1,lista2):
+    sumatoria=0
+    for x,y in zip(lista1,lista2):  # con el zip se van a juntar las coordenadas x de la lista uno con las coordenadas x de la lista 2 y lo mismo para y
+        sumatoria +=  (x-y) ** 2
+    return math.sqrt(sumatoria)
+
+# Definir la función de clasificación 
+def clasificacion(testlist,traininglist,traininglabel,K):
+    distancias = []
+    for traininglist, label in zip(traininglist,traininglabel):
+        distancias.append(distancia(testlist,traininglist),label)
+        distancias.sort(key=itemgetter(0))
+        votelabels=[]
+        for x in distancias[:K]:
+            votelabels.append(x[1])
+        return mode(votelabels)
